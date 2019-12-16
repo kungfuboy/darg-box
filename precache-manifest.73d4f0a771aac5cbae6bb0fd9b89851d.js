1bf7b5d33e82d33b7f42bb3a1836691380a41286
songjunjie <songjunjie@ygsoft.com>
2019-12-16 15:50:04 +0800

    deploy

---
+++ precache-manifest.73d4f0a771aac5cbae6bb0fd9b89851d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "716e249cd3c2095c7c5d",
+    "url": "/darg-box/static/css/main.fde0b023.chunk.css"
+  },
+  {
+    "revision": "716e249cd3c2095c7c5d",
+    "url": "/darg-box/static/js/main.716e249c.chunk.js"
+  },
+  {
+    "revision": "604dacc94ff142ca2b4a",
+    "url": "/darg-box/static/js/1.604dacc9.chunk.js"
+  },
+  {
+    "revision": "c7ca7f047813143f5c44",
+    "url": "/darg-box/static/js/runtime~main.c7ca7f04.js"
+  },
+  {
+    "revision": "5de4bf2a1ac306c02fd7ea24fb523621",
+    "url": "/darg-box/index.html"
+  }
+];
